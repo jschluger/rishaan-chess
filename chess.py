9f3c8ff712ddda6ac1470d274e906f4376ff1dad
Jack Schluger <jes543@cornell.edu>
2021-06-07 13:36:07 -0400

    05.17.21

--- chess.py
+++ chess.py
@@ -1,39 +1,48 @@
 from pprint import pprint
 
+
 class ChessGame():
-  def __init__(self):
-    """
+    def __init__(self):
+        """
     Invariants:
     * self.board[x][y] contains the piece on square (x,y)
     * self.board has 2 kings (unless game over)
     * every piece that hasn't been taken is somewhere in self.board
     * if there is no piece on square (x,y), then self.board[x][y] == None
     """
-    self.board = [[None] * 8] * 8
-
-    """
+        self.board = [[None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None],
+                      [None, None, None, None, None, None, None, None]]
+        """
     Invariants: 
     * (self.turn == True)  <==> White's Turn
     * (self.turn == False) <==> Black's Turn
     """
-    self.turn = True
-    self.place_pieces()
+        self.turn = True
 
 
-    def place_pieces(self):
-      self.board[1][2] = Piece()
+class WPawn():
+    def __init__(self, x, y, game):
+        self.x = x
+        self.y = y
+        self.game = game
+        game.board[x][y] = self
 
-class Piece():
-  def __init__(self):
-    """
-    Invariants: 
-    * 
-    """
-    self.possible_moves = _____________
+    def get_valid_moves(self):
+        want_to_check = [(self.x, self.y + 1), (self.x + 1, self.y + 1),
+                         (self.x - 1, self.y + 1)]
+        print('want_to_check[0]', want_to_check[0])
+        result = []
+        if self.game.board[want_to_check[0][0]][want_to_check[0][1]] == None:
+            result.append(want_to_check[0])
 
-    """
-    Invariants: 
-    * 
-    """
-    self.possible_takes = _____________
+        # ToDo (Rishaan's HW): finish writing this method to consider the rest of
+        # the cases, and append any more valid places this pawn can move to result
 
+        print('going to return', result)
+        return result
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-import chess 
+from chess import *
+from pprint import pprint
 
 if __name__ == '__main__':
-  print('test main')
-  game = chess.ChessGame()
-  
+    print('test main')
+    game = ChessGame()
+    pawn1 = WPawn(1, 1, game)
+    pprint(game.board)
+
+    pawn1.get_valid_moves()
